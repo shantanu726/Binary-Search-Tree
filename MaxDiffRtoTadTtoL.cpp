4480c3c0b04a9bc711a5d1a8ebe1ee002fa4a3e9
shantanu sahoo <126313576+shantanu726@users.noreply.github.com>
2023-03-24 00:44:38 +0530

    Update MaxDiffRtoTadTtoL.cpp

--- MaxDiffRtoTadTtoL.cpp
+++ MaxDiffRtoTadTtoL.cpp
@@ -106,4 +106,70 @@ int main() {
         cout << endl;
     }
 }
-// } Driver Code Ends
+
+
+/*
+int x = -1 ;
+     void solve( Node *root , int target , Node*&node , int curr )
+    {
+        if(!root)
+        {
+            return ;
+        }
+        
+        if( root->data == target )
+        {
+            node = root ;
+            x = curr ;
+            return ;
+        }
+        
+        if( root->data > target )
+        {
+            solve( root->left , target , node ,  curr + root->data ) ;
+        }
+        else{
+           solve( root->right , target , node ,  curr + root->data ) ;
+        }
+        
+        
+        
+    }
+    
+    int solve2( Node *root )
+    {
+        if(!root->left && !root->right )
+        {
+            return root->data ;
+        }
+        
+        if(!root->left && root->right )
+        {
+            return root->data + solve2( root->right )  ;
+        }
+        
+        if(root->left && !root->right )
+        {
+            return root->data + solve2( root->left )  ;
+        }
+        
+        return min( root->data + solve2( root->left ) , root->data + solve2( root->right ) ) ;
+        
+    }
+    int maxDifferenceBST(Node *root,int target){
+        // Code here
+        
+        Node *node = NULL ;
+       
+        
+        solve( root , target , node ,  0 ) ;
+        
+        if( node == NULL ) return -1 ;
+        
+        int ans = INT_MIN ;
+        
+        int mn = solve2(node) ;
+        
+        return x - mn + target  ;
+    }
+    */
